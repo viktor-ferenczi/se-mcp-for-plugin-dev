2d27f83ecbd4d9428c914afb74a0491e8d44de98
Viktor Ferenczi <viktor@ferenczi.eu>
2025-11-26 22:51:16 +0100

    Copying indexable game content

--- Prepare.bat
+++ Prepare.bat
@@ -6,7 +6,7 @@ if %ERRORLEVEL% NEQ 0 goto failed
 echo ==========
 :skip1
 
-if exist VRage.XmlSerializers goto skip2
+if exist Decompiled\VRage.XmlSerializers goto skip2
 call Decompile.bat
 if %ERRORLEVEL% NEQ 0 goto failed
 echo ==========
@@ -14,7 +14,13 @@ echo ==========
 
 rmdir /s /q Bin64
 
-if exist code-index-mcp\fastmcp.json goto skip3
+if exist Decompiled\Content goto skip3
+echo Copying indexable content
+python -u copy_content.py
+if %ERRORLEVEL% NEQ 0 goto failed
+:skip3
+
+if exist code-index-mcp\fastmcp.json goto skip4
 git submodule update --init --recursive
 if %ERRORLEVEL% NEQ 0 goto failed
 cd code-index-mcp
@@ -23,7 +29,7 @@ uv sync
 if %ERRORLEVEL% NEQ 0 goto failed
 cd ..
 if %ERRORLEVEL% NEQ 0 goto failed
-:skip3
+:skip4
 
 echo Writing MCP configuration file: mcp.json
 python -u mcp_config.py
