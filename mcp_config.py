bff4d5ee84d7fa89c404d89fff6d99f77e96f96b
Viktor Ferenczi <viktor@ferenczi.eu>
2025-12-03 22:53:38 +0100

    Auto-approving all tools

--- mcp_config.py
+++ mcp_config.py
@@ -8,9 +8,31 @@ CODE_DIR = os.path.join(CWD, 'Decompiled')
 config = {
     "mcpServers": {
         "space-engineers-code-index": {
+            "timeout": 60,
+            "type": "stdio",
             "command": "uvx",
-            "args": ["code-index-mcp", "--project-path", CODE_DIR],
-        },
+            "args": [
+                "code-index-mcp",
+                "--project-path",
+                CODE_DIR
+            ],
+            "cwd": CWD,
+            "autoApprove": [
+                "set_project_path",
+                "search_code_advanced",
+                "find_files",
+                "get_file_summary",
+                "refresh_index",
+                "build_deep_index",
+                "get_settings_info",
+                "create_temp_directory",
+                "check_temp_directory",
+                "clear_settings",
+                "refresh_search_tools",
+                "get_file_watcher_status",
+                "configure_file_watcher"
+            ]
+        }
     }
 }
 
@@ -23,10 +45,10 @@ with open(fastmcp_config_path, 'rt') as f:
 
 fastmcp_config['name'] = 'Space Engineers Code Index'
 fastmcp_config['description'] = (
-    "Index of the decompiled Space Engineers code. " +
-    "It includes CSharp code (*.cs), the corresponding ILCode (*.il) and " +
-    "any textual content in the game's Content folder (*.gsc, *.hlsi, *.json, *.mwl, *.resx, *.sbc, *.sbl, *.scf, *.vs, *.vsc, *.xml). " +
-    "Use the ILCode only for transpiler patches and pre-patches."
+        "Index of the decompiled Space Engineers code. " +
+        "It includes CSharp code (*.cs), the corresponding ILCode (*.il) and " +
+        "any textual content in the game's Content folder (*.gsc, *.hlsi, *.json, *.mwl, *.resx, *.sbc, *.sbl, *.scf, *.vs, *.vsc, *.xml). " +
+        "Use the ILCode only for transpiler patches and pre-patches."
 )
 
 with open(fastmcp_config_path, 'wt') as f:
